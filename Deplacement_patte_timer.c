4553576251889e4d34826b7734c53f74492539bf
jgaltier98 <47536336+jgaltier98@users.noreply.github.com>
2019-06-20 19:31:24 +0200

    Add files via upload

--- Deplacement_patte_timer.c
+++ Deplacement_patte_timer.c
@@ -8,44 +8,40 @@
 #include "GPIO.h"
 
 //adresse des cartes filles
-#define UN 1
-#define DEUX 7
-#define TROIS 3
-#define QUATRE 9
-#define CINQ 5
-#define SIX 6
+#define ADDR_UN 1
+#define ADDR_DEUX 7
+#define ADDR_TROIS 3
+#define ADDR_QUATRE 9
+#define ADDR_CINQ 5
+#define ADDR_SIX 6
 
-
+//Fluidité des mouvements de marche
 #define fluidity 4 //2 fonctionne mais le temps de réponses de moteur étant aléatoire étant donné leur âge....
-
-#define PWMPRESCALE (25-1) //25 PCLK cycles to increment TC by 1 i.e. 1 Micro-second
 
 CAN_MSG MsgBuf_TX1, MsgBuf_TX2; /* TX and RX Buffers for CAN message */
 CAN_MSG MsgBuf_RX1, MsgBuf_RX2; /* TX and RX Buffers for CAN message */
 volatile uint32_t CAN1RxDone = FALSE, CAN2RxDone = FALSE;
-
+//Déclaration fonctions d'initialisation
 void initPWM(void);
 void CAN_Initialisation(void);
 void Send_CAN(int ID, int DataA, int DataB);
 void send_position(int ID, int positionA, int positionB, int positionC);
-
-void test_servo(short patte);
-void test_all_servo(void);
-
+//Déclaration des variables et tableaux
+short tab_addr_patte[6]={ADDR_UN,ADDR_DEUX,ADDR_TROIS,ADDR_QUATRE,ADDR_CINQ,ADDR_SIX};
+short addr_index=0;
 int tps_timer0=0,flag_timer0=0;
 int tps_timer1=0,flag_timer1=0;
-int k=0,patte=0,testeur=0;
-// DataA : paquet le plus lourd. Initialisation du Baudarate à 125kBps
-int servo[6][3];//npatte et n_servo
-
+int k=0;
+char get_octet_from_bt;
+int servo[6][3];//nombre_de_patte et n_servo
 char etat_deplacement=0,etat=0;
-char poubelle[2]={0};
 short i=0,angle,distance,delay_rs;
 char qualite, angle1,  angle2,  distance1 ,  distance2;
 short diametre1 = 300, diametre2 = 600;
 char tableau_reception_lidar[20]={0};
-
-short test_bluetooth(void);
+//Déclaration des fonctions utilisées par l'hexapode
+void test_servo(short patte);
+void test_all_servo(void);
 void send_BT(char * str,short nb_char);
 void delay(int seconde);
 void delay2(int seconde);
@@ -57,20 +53,18 @@ void retract_stand(void);
 void default_position(void);
 
 void TIMER0_IRQHandler(void) {  
-	//int i=0;
 	LPC_TIM0->IR |= 1<<0;   	// baisse Flag associé à MR0
-	tps_timer0+=1;
-	if(tps_timer0==100){
+	tps_timer0+=1;						//On compte les millisecondes
+	if(tps_timer0==100){			//On attend 100 millisecondes
 		tps_timer0=0;
 		flag_timer0=1;
 	}
 }
 
 void TIMER1_IRQHandler(void) {  
-	//int i=0;
 	LPC_TIM1->IR |= 1<<0;   	// baisse Flag associé à MR0
 	tps_timer1+=1;
-	if(tps_timer1==10){
+	if(tps_timer1==10){				//On attend 10 millisecondes
 		tps_timer1=0;
 		flag_timer1=1;
 	}
@@ -96,79 +90,90 @@ void Timer012_Init(unsigned int prescaler, unsigned int valeur){
 }
 // Programme principal
 int main(void) {
+	//Configuration du registre des LEDs
 	LPC_GPIO1->FIODIR0 |= 0x13;
 	LPC_GPIO1->FIODIR1 |= 0xC7;	
-	
+	//Fin configuration du registre des leds
+	//Initialisations des périphériques
 	Init_UART();
 	initPWM();	
 	CAN_Initialisation();
 	Timer012_Init(0, 32767);  	// prescaler = 0+1  diviseur -> 1s //32767 pour 1 ms
 	NVIC_EnableIRQ(TIMER0_IRQn); 		// validation IT pour Timer 0 (bit 1)	
 	NVIC_EnableIRQ(TIMER1_IRQn); 		// validation IT pour Timer 1 (bit 1)
-	
+	//Fin initialisation des périphériques
+	//Test led
 	LPC_GPIO1->FIOPIN0 |= 0x13;
 	LPC_GPIO1->FIOPIN1 |= 0xC7;
 	delay(5);
 	LPC_GPIO1->FIOPIN0 = 0x00;
 	LPC_GPIO1->FIOPIN1 = 0x00;
-	
+	//Fin test led
+	//Position par défaut des pattes
 	default_position();
-	delay(20);
-	
+	delay(20); //attentes 2 secondes
+	//Trames d'activations du lidar
 	UART_LIDAR_PutChar(0xA5);
 	for(delay_rs=0;delay_rs<100;delay_rs++);
 	UART_LIDAR_PutChar(0x20);
-	///////test bluetooth
-	UART_BT_PutChar('W');
-	while(1){
-		send_BT("AT+ROLE=<Param>",15);
-		delay(1);
-		send_BT("AT+ROLE?\r\n",9);
-		delay(10);
-		
-		
-	}/*{
-		if(test_bluetooth()==1){
-			LPC_GPIO1->FIODIR0 |= 0x13;
-			LPC_GPIO1->FIODIR1 |= 0xC7;	
-		}
-	}*/
-//while(1);
-	///////fin test
+	//#####################################
+	//#         Codes principales         #
+	//#####################################
 	while(1)
 	{
+		//Partie récupération des caractères du Bluetooth
+		get_octet_from_bt = UART_BT_GetChar();
+		if(get_octet_from_bt=='A'){	
+			send_BT("test_servos",11);
+			etat = 7;//testservo
+		}
+		else if(get_octet_from_bt=='B'){	
+			send_BT("lidar_mode",10);
+			etat = 0;//mode lidar
+		}
+		else if(get_octet_from_bt=='C'){	
+			send_BT("stop",4);
+			etat = 8;//arret
+		}
+		else if(get_octet_from_bt=='D'){	
+			send_BT("rotation_mode",13);
+			etat = 9;//mode rotation
+		}
+		//Fin partie récupération des caractères du Bluetooth
+		//Début automate de contrôle de l'hexapode
 		switch(etat){
 			case 0:	
+					//Extinction des LEDs
 					LPC_GPIO1->FIOPIN0 = 0x00;
 					LPC_GPIO1->FIOPIN1 = 0x00;
+					//LEDs correspondantes  à l'état 0
 					LPC_GPIO1->FIOPIN0 |= 0x01;
+				  //Récupération de la trame envoyée par le lidar
 					tableau_reception_lidar[0] = UART_LIDAR_GetChar();
 					tableau_reception_lidar[1] = UART_LIDAR_GetChar();
 					tableau_reception_lidar[2] = UART_LIDAR_GetChar();
 					tableau_reception_lidar[3] = UART_LIDAR_GetChar();
 					tableau_reception_lidar[4] = UART_LIDAR_GetChar();
 				
-					//decodage de la trame pour obtenir les valeurs de qualité, distance, angle :
+					//Decodage de la trame pour obtenir les valeurs de qualité, distance, angle :
 					//qualite = bit [7 à 2] de la trame 0
 					qualite = tableau_reception_lidar[0];
 					qualite = (qualite >>2);
+					//Angle dans lequel est repérer l'obstacle
 					//angle = bit [14 à 7] de la trame 2 et bit [7 à 1] de la trame 1
 					angle1= tableau_reception_lidar[1];
 					angle2= tableau_reception_lidar[2];
 					//valeur angle par concaténation [14:0]
 					angle=((angle2<<7) | (angle1)>>1) / 64.00; // angle en degrés
-
-
+					//Récupération de la distance à laquelle est repérer l'obstacle
+					//distance = bit [15 à 8] de la trame 4 et bit [7 à 0] de la trame 3
 					distance1 = tableau_reception_lidar[3];
 					distance2 = tableau_reception_lidar[4];
-
-					//distance = bit [15 à 8] de la trame 4 et bit [7 à 0] de la trame 3
-
 					//valeur angle par concaténation [15:0]
 					distance= ((distance2<<8) | distance1)/4.00;  //distance en mm
-					//machine à état pour effectuer un déplacement en fonction des donnees lidar
-					//do
-					//Marche avant
+					//Fin récupération de la trame envoyée par le lidar
+					//##################################################
+					//Conditions et instructions selon les informations récupérées
 					if ((distance <= diametre2) && (distance>100)){
 						if ((angle <= 359) && (angle>=301))	//OBSTACLE EN AVANT GAUCHE
             {
@@ -234,6 +239,16 @@ int main(void) {
 				marche(1);//4
 				etat = 0;
 		 break;
+		 case 7:
+			 test_servo(tab_addr_patte[addr_index]);
+			 addr_index++;
+			 if(addr_index>5) addr_index = 0;
+		 break;
+		 case 8:
+		 break;
+		 case 9:
+			 rotation(1);
+		 break;
      default:
 				LPC_GPIO1->FIOPIN0 |= 0x13;
 				LPC_GPIO1->FIOPIN1 |= 0xC7;
@@ -247,65 +262,65 @@ return 0;
 
 
 void marche(short direction){
+	//Fonction permettant à l'hexapode d'effectuer une fois l'action marche
 	short deux,quatre,six;
 	short un,trois,cinq;
-	
+	//Configuration des pattes suivants la direction choisi
 	if(direction==1){
-		deux = UN;
-		quatre = TROIS;
-		six = CINQ;
+		deux = ADDR_UN;
+		quatre = ADDR_TROIS;
+		six = ADDR_CINQ;
 		
-		un = SIX;
-		trois = DEUX;
-		cinq = QUATRE;
+		un = ADDR_SIX;
+		trois = ADDR_DEUX;
+		cinq = ADDR_QUATRE;
 	}
 	else if (direction==2){
-		deux = DEUX;//2
-		quatre = QUATRE;//4
-		six = SIX;//6
+		deux = ADDR_DEUX;
+		quatre = ADDR_QUATRE;
+		six = ADDR_SIX;
 		
-		un = UN;//1
-		trois = TROIS;//3
-		cinq = CINQ;//5
+		un = ADDR_UN;
+		trois = ADDR_TROIS;
+		cinq = ADDR_CINQ;
 	}
 	else if (direction==3){
-		deux = TROIS;
-		quatre = CINQ;
-		six = UN;
+		deux = ADDR_TROIS;
+		quatre = ADDR_CINQ;
+		six = ADDR_UN;
 		
-		un = DEUX;
-		trois = QUATRE;
-		cinq = SIX;
+		un = ADDR_DEUX;
+		trois = ADDR_QUATRE;
+		cinq = ADDR_SIX;
 	}
 	else if (direction==4){
-		deux = QUATRE;
-		quatre = SIX;
-		six = DEUX;
+		deux = ADDR_QUATRE;
+		quatre = ADDR_SIX;
+		six = ADDR_DEUX;
 		
-		un = TROIS;
-		trois = CINQ;
-		cinq = UN;
+		un = ADDR_TROIS;
+		trois = ADDR_CINQ;
+		cinq = ADDR_UN;
 	}
 	else if (direction==5){
-		deux = CINQ;
-		quatre = UN;
-		six = TROIS;
+		deux = ADDR_CINQ;
+		quatre = ADDR_UN;
+		six = ADDR_TROIS;
 		
-		un = QUATRE;
-		trois = SIX;
-		cinq = DEUX;
+		un = ADDR_QUATRE;
+		trois = ADDR_SIX;
+		cinq = ADDR_DEUX;
 	}
 	else if (direction==6){
-		deux = SIX;
-		quatre = DEUX;
-		six = QUATRE;
+		deux = ADDR_SIX;
+		quatre = ADDR_DEUX;
+		six = ADDR_QUATRE;
 		
-		un = CINQ;
-		trois = UN;
-		cinq = TROIS;
+		un = ADDR_CINQ;
+		trois = ADDR_UN;
+		cinq = ADDR_TROIS;
 	}
-	//direction vers l'avant = patte 2
-	//mise en position 2 4 6 ################################################
+	//mise en position 2 4 6 ##########################################
 	send_position(deux,150,70,210); 
 	send_position(quatre,150,70,210); 
 	send_position(six,150,70,210); 
@@ -322,24 +337,21 @@ void marche(short direction){
 	send_position(quatre,190,150,150); //direction+2
 	send_position(six,110,150,150); //direction+4
 	
-	//########################################################################
+	//#################################################################
 	//préparation pour mouvement 1 3 5#################################
 	delay(fluidity);
 	
 	send_position(un,150,70,210); //direction-1
 	send_position(trois,150,70,210); //direction+2-1
 	send_position(cinq,150,70,210); //direction+4-1
-	//#################################################
-	
-	
-	
-	//imprimer mouvement 2 4 6####################################################
+	//#################################################################
+	//imprimer mouvement 2 4 6#########################################
 	delay(fluidity);
 	send_position(deux,150,150,150);
 	send_position(quatre,150,150,150);
 	send_position(six,150,150,150);
-	//##########################################
-	//fin préparation mouvement 1 3 5#####################################
+	//#################################################################
+	//fin préparation mouvement 1 3 5##################################
 	delay(fluidity);
 	
 	send_position(un,150,150,150);
@@ -347,9 +359,10 @@ void marche(short direction){
 	send_position(cinq,150,150,150);
 	
 	delay(fluidity);
-	//######################################################################
+	//#################################################################
 }
 void rotation(int nombre_de_tour){
+	//Fonction permettant à l'hexapode d'effectuer une fois l'action rotation
 	int tour=0;
 	for(tour=0;tour<nombre_de_tour;tour++){
 		synchro_patte_1_3_5(150,70,190);
@@ -383,24 +396,28 @@ void rotation(int nombre_de_tour){
 }
 
 void synchro_patte_2_4_6(int pos_1,int pos_2,int pos_3){
-	send_position(DEUX,pos_1,pos_2,pos_3);	
-	send_position(QUATRE,pos_1,pos_2,pos_3);
-	send_position(SIX,pos_1,pos_2,pos_3);
+	//Fonction permettant de synchroniser les pattes 2,4 et 6
+	send_position(ADDR_DEUX,pos_1,pos_2,pos_3);	
+	send_position(ADDR_QUATRE,pos_1,pos_2,pos_3);
+	send_position(ADDR_SIX,pos_1,pos_2,pos_3);
 }
 void synchro_patte_1_3_5(int pos_1,int pos_2,int pos_3){
-	send_position(UN,pos_1,pos_2,pos_3);
-	send_position(TROIS,pos_1,pos_2,pos_3);
-	send_position(CINQ,pos_1,pos_2,pos_3);
+	//Fonction permettant de synchroniser les pattes 1,3 et 5
+	send_position(ADDR_UN,pos_1,pos_2,pos_3);
+	send_position(ADDR_TROIS,pos_1,pos_2,pos_3);
+	send_position(ADDR_CINQ,pos_1,pos_2,pos_3);
 }
 
-void delay(int seconde){
-	for(k=0;k<seconde;k++){
+void delay(int centieme_seconde){
+	//Attend le nombre de centième de secondes passé en paramètre
+	for(k=0;k<centieme_seconde;k++){
 		while(flag_timer0==0);
 		flag_timer0=0;
 	}
 }
-void delay2(int seconde){
-	for(k=0;k<seconde;k++){
+void delay2(int dixieme_seconde){
+	//Attend le nombre de dixième de secondes passé en paramètre
+	for(k=0;k<dixieme_seconde;k++){
 		while(flag_timer1==0);
 		flag_timer1=0;
 	}
@@ -442,27 +459,30 @@ void send_position(int ID, int positionA, int positionB, int positionC){
 
 
 void retract_stand(void){
-	send_position(UN,150,70,210);//1
-	send_position(DEUX,150,70,210);//2
-	send_position(TROIS,150,70,210);//3
+	//L'hexapode rétracte toutes ses pattes
+	send_position(ADDR_UN,150,70,210);//1
+	send_position(ADDR_DEUX,150,70,210);//2
+	send_position(ADDR_TROIS,150,70,210);//3
 	delay(1);
-	send_position(QUATRE,150,70,210);//4
-	send_position(CINQ,150,70,210);//5
-	send_position(SIX,150,70,210);//6
+	send_position(ADDR_QUATRE,150,70,210);//4
+	send_position(ADDR_CINQ,150,70,210);//5
+	send_position(ADDR_SIX,150,70,210);//6
 }
 
 void default_position(void){
-	send_position(UN,150,150,150);
-	send_position(DEUX,150,150,150);
-	send_position(TROIS,150,150,150);
+	//Position par défaut de l'hexapode
+	send_position(ADDR_UN,150,150,150);
+	send_position(ADDR_DEUX,150,150,150);
+	send_position(ADDR_TROIS,150,150,150);
 	delay(1);
-	send_position(QUATRE,150,150,150);
-	send_position(CINQ,150,150,150);
-	send_position(SIX,150,150,150);
+	send_position(ADDR_QUATRE,150,150,150);
+	send_position(ADDR_CINQ,150,150,150);
+	send_position(ADDR_SIX,150,150,150);
 }
 void test_servo(short patte){
+	//Test la patte dont l'adresse est passée en paramètre
 	short i=0,j=0;
-	short tab[3]={70};
+	short tab[3]={70}; //mise en position des servos à leurs positions la plus faible
 	for(i=0;i<3;i++){
 		for(j=70;j<211;j++){
 			tab[i]=j;
@@ -471,15 +491,20 @@ void test_servo(short patte){
 		}
 		tab[i]=150;
 	}
+	tab[0]=150;
+	tab[1]=150;
+	tab[2]=150;
+	send_position(patte,tab[0],tab[1],tab[2]);
 }
 
 void test_all_servo(void){
-		test_servo(UN);
-		test_servo(DEUX);
-		test_servo(TROIS);
-		test_servo(QUATRE);
-		test_servo(CINQ);
-		test_servo(SIX);
+	//Test toutes les pattes
+		test_servo(ADDR_UN);
+		test_servo(ADDR_DEUX);
+		test_servo(ADDR_TROIS);
+		test_servo(ADDR_QUATRE);
+		test_servo(ADDR_CINQ);
+		test_servo(ADDR_SIX);
 	
 	
 }
@@ -511,21 +536,9 @@ void initPWM(void){
 	
 }
 
-short test_bluetooth(void){
-	short i,valid = 0;
-	char rec_tab[3];
-	char quit_tab[10]="---<enter>";
-	send_BT("$$$",3);
-	delay(11);
-	for(i=0;i<3;i++)rec_tab[i]=UART_BT_GetChar();
-	if(rec_tab[0]=='C' && rec_tab[0]=='M' && rec_tab[0]=='D'){
-		send_BT("---<enter>",10);
-		valid = 1;
-	}
-	
-	return valid;
-}
 void send_BT(char * str,short nb_char){
+	//Envoie la série de caractère passé en paramètre
+	//nb_char indique le nombre de caractère de la chaine
 	short i;
 	for(i=0;i<nb_char;i++){
 		UART_BT_PutChar(str[i]);
